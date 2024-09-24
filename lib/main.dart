87b1d5ad8c4bea96b72ea592777f23c287f4d98c
mjk22071998 <mjk22071998@gmail.com>
2024-09-24 18:24:50 +0500

    API key hidden

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:todo_supabase/session/session_manager.dart';
 
 const supabaseUrl = 'https://kvjtrrupyptymklnvkow.supabase.co';
 const supabaseKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2anRycnVweXB0eW1rbG52a293Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjgxMTk0MywiZXhwIjoyMDQyMzg3OTQzfQ.d9qIejsduKM9GuL81UMBYz48NrWqArfd2tR4oXTsiGs';
+    'API KEY';
 
 Future<void> main() async {
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
